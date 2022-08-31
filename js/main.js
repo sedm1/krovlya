ed22bd6d74422446ea3fafb247bfe8f9d44217c2
Николай <novikovn383@gmail.com>
2022-08-31 21:23:47 +0500

    product

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ $(document).mouseup(function(e) {
       $(".menu").removeClass("menu-active");
     }
 });
+$("ul.tabs__caption").on("click", "li:not(.active)", function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active")
+    .closest("div.tabs")
+    .find("div.tabs__content")
+    .removeClass("active")
+    .eq($(this).index())
+    .addClass("active");
+});
